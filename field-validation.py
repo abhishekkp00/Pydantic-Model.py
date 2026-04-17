ecebe73b89d8800376b6c8755d9f34779e8a65fc
abhishekkp00 <abhishekforcollege@gmail.com>
2026-04-17 22:00:51 +0530

    field-validator

---
+++ field-validation.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, field_validator, model_validator
+
+
+class User(BaseModel):
+    username: str
+
+
+    @field_validator('username')
+    def username_length(cls, value):
+        if len(value) < 3:
+            raise ValueError('Username must be at least 3 characters long')
+        return value
+    
+
+class SignupData(BaseModel):
+    password: str
+    confirm_password: str
+
+    @model_validator(mode='after')
+    def passwords_match(cls, values):
+        if values.password != values.confirm_password:
+            raise ValueError('Passwords do not match')
+        return values
